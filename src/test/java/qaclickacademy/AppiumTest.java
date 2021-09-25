0613abdf5a5e267b70d508a47065850a8f412ece
kaustuv mitra <kaustuvmitra@gmail.com>
2021-09-25 14:54:16 -0400

    commit message

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,26 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppiumTest 
+{
+    
+	//@Test
+	public void AppTest() {
+		
+		System.out.println("AppTest");
+    	
+    }
+	
+	@Test
+	public void AppTest1() {
+		
+		System.out.println("AppTest1");
+		
+	}
+    
+    
+}
---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest 
+{
+   
+	@Test
+	public void RestAPITest1() {
+		
+		System.out.println("RestAPITest1");
+		
+	}
+	
+	@Test
+	public void RestAPITest2() {
+		
+		System.out.println("RestAPITest2");
+		
+	}
+	
+}
